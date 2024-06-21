08e947fe8aff3607b30235dcf209f9206476f2fe
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2024-06-21 13:18:12 -0300

    :bulb: comments in the source code

--- src/main/java/org/example/storage/FileManager.java
+++ src/main/java/org/example/storage/FileManager.java
@@ -5,8 +5,24 @@ import org.example.system.task.Task;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Tha interface FileManager store and retrieve tasks
+ */
 public interface FileManager {
 
+    /**
+     * Read all tasks
+     *
+     * @return Task list
+     * @throws IOException if an I/O error occurs
+     */
     List<List<String>> read() throws IOException;
+
+    /**
+     * Store all task
+     *
+     * @param tasks Task list for store
+     * @throws IOException if an I/O error occurs
+     */
     void writeAllTasks(List<Task> tasks) throws IOException;
 }
--- src/main/java/org/example/view/View.java
+++ src/main/java/org/example/view/View.java
@@ -1,5 +1,12 @@
 package org.example.view;
 
+/**
+ *  This interface is responsible is the view
+ */
 public interface View {
+
+    /**
+     * Run the application
+     */
     void runApp();
 }
